a3bae321292c67b5355bd235019930664206e331
pbergqui <pbergqui@Unni>
2015-10-15 22:04:55 +0200

    Gjør en endring

--- src/testgetpdubody/TestGetPDUbody.java
+++ src/testgetpdubody/TestGetPDUbody.java
@@ -17,7 +17,7 @@ public class TestGetPDUbody {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hello world!");
-        // tro om dette fungerer?
+        // tro om dette fungerer? Endret
     }
     
 }
